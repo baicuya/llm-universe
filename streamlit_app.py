9ea118acb64f0194414822d19902adf9f991605e
baicuya <81811911+baicuya@users.noreply.github.com>
2024-04-24 20:39:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import sys
 # sys.path.append("../C3 搭建知识库") # 将父目录放入系统路径中
-from zhipuai_embedding import ZhipuAIEmbeddings
+# from zhipuai_embedding import ZhipuAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain import OpenAI, LLMChain, PromptTemplate
@@ -31,82 +31,11 @@ def generate_response(input_text):
     #st.info(output)
     return output
 
-# def generate_memory_response(question:str):
-#     # llm = ChatOpenAI(model_name = "gpt-3.5-turbo", temperature = 0,openai_api_key = openai_api_key)
-#     memory = ConversationBufferMemory(
-#         memory_key="chat_history",  # 与 prompt 的输入变量保持一致。
-#         return_messages=True  # 将以消息列表的形式返回聊天记录，而不是单个字符串
-#     )
-#     # retriever=vectordb.as_retriever()
-#     qa = ConversationChain(
-#         llm=llm, 
-#         verbose=False, 
-#         memory=ConversationBufferMemory()
-#     )
-#     result = qa({"question": question})
-#     return result['answer']
-
-def get_vectordb():
-    # 定义 Embeddings
-    embedding = ZhipuAIEmbeddings()
-    # 向量数据库持久化路径
-    persist_directory = '/Users/baicu/llm-universe/data_base/vector_db/chroma'
-    # 加载数据库
-    vectordb = Chroma(
-        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
-        embedding_function=embedding
-    )
-    return vectordb
-
-#带有历史记录的问答链
-def get_chat_qa_chain(question:str):
-    vectordb = get_vectordb()
-    # llm = ChatOpenAI(model_name = "gpt-3.5-turbo", temperature = 0,openai_api_key = openai_api_key)
-    # llm = ChatZhipuAI(model="glm-4", temperature=0, api_key=api_key)
-    memory = ConversationBufferMemory(
-        memory_key="chat_history",  # 与 prompt 的输入变量保持一致。
-        return_messages=True  # 将以消息列表的形式返回聊天记录，而不是单个字符串
-    )
-    retriever=vectordb.as_retriever()
-    qa = ConversationalRetrievalChain.from_llm(
-        llm,
-        retriever=retriever,
-        memory=memory
-    )
-    result = qa({"question": question})
-    return result['answer']
-
-#不带历史记录的问答链
-def get_qa_chain(question:str):
-    vectordb = get_vectordb()
-    # llm = ChatOpenAI(model_name = "gpt-3.5-turbo", temperature = 0,openai_api_key = openai_api_key)
-    # llm = ChatZhipuAI(model="glm-4", temperature=0, api_key=api_key)
-    template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
-        案。最多使用三句话。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
-        {context}
-        问题: {question}
-        """
-    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],
-                                 template=template)
-    qa_chain = RetrievalQA.from_chain_type(llm,
-                                       retriever=vectordb.as_retriever(),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
-    result = qa_chain({"query": question})
-    return result["result"]
-
 
 # Streamlit 应用程序界面
 def main():
     st.title('🌙(>^ω^<) your assistant(>^ω^<)🌙')
     # zhipuai_api_key = st.sidebar.text_input('ZHIPUAI_API_KEY', type='password')
-
-    # 添加一个选择按钮来选择不同的模型
-    #selected_method = st.sidebar.selectbox("选择模式", ["qa_chain", "chat_qa_chain", "None"])
-    selected_method = st.radio(
-        "你想选择哪种模式进行对话？",
-        ["chat", "qa_chain", "qa_chain&chat"],
-        captions = ["chat",  "检索问答模式", "检索问答模式&chat"])
 
     # 用于跟踪对话历史
     if 'messages' not in st.session_state:
@@ -120,12 +49,6 @@ def main():
         if selected_method == "chat":
             # 调用 respond 函数获取回答
             answer = generate_response(prompt)
-        # elif selected_method == "qa_memory":
-        #     answer = generate_memory_response(prompt)
-        elif selected_method == "qa_chain":
-            answer = get_qa_chain(prompt)
-        elif selected_method == "qa_chain&chat":
-            answer = get_chat_qa_chain(prompt)
 
         # 检查回答是否为 None
         if answer is not None:
